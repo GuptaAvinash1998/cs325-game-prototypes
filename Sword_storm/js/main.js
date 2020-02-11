4d1c1beaf1aa541948efc588ae76567293db5777
Avinash Gupta <avig1998@gmail.com>
2020-02-11 13:07:26 -0500

    Second commit

--- Sword_storm/js/main.js
+++ Sword_storm/js/main.js
@@ -1,4 +1,9 @@
+var sword;
+var man;
+var platform;
+
 var mainState = { //create the main state of the gamed
+
 
     preload:function () { //This is used to load the images and the sounds and is called at the beginning
 
@@ -6,7 +11,6 @@ var mainState = { //create the main state of the gamed
         game.load.image('Sword', 'assets/sword_sprite.png'); //loads the sword
         game.load.image('Surface', 'assets/platform.png');
         game.load.audio('Play', 'assets/background_music.mp3'); //loads the jump music
-        //game.load.audio('Dead', 'assets/kaboom.wav'); //loads the dead music
     },
 
     create:function () { //This is used to set up the game, display the sprites, etc and is called after preload
@@ -15,20 +19,21 @@ var mainState = { //create the main state of the gamed
 
         game.physics.startSystem(Phaser.Physics.ARCADE); //set the physics system
 
-        this.man = game.add.sprite(100, 304, 'Little_buddy'); //this sets the bird sprite at position (100, 245)
+        man = game.add.sprite(100, 304, 'Little_buddy'); //this sets the bird sprite at position (100, 245)
 
-        this.sword = game.add.sprite(10, -50, 'Sword');
+        sword = game.add.sprite(10, -50, 'Sword');
 
-        game.physics.arcade.enable(this.sword);
-        game.physics.arcade.enable(this.man);
 
-        this.sword.body.gravity.y = 450;
+        game.physics.arcade.enable(sword);
+        game.physics.arcade.enable(man);
 
-        this.platform = game.add.image(0, 360, 'Surface');
+        sword.body.gravity.y = 450;
 
-        game.physics.arcade.enable(this.man); //adds physics to the bird
-        game.physics.arcade.enable(this.sword); //adds physics to the bird
-        game.physics.arcade.enable(this.platform); //adds physics to the bird
+        sword.body.setSize(32, 48, true);
+
+        platform = game.add.image(0, 360, 'Surface');
+
+        game.physics.arcade.enable(platform); //adds physics to the bird
 
         this.cursor = game.input.keyboard.createCursorKeys();
 
@@ -36,42 +41,34 @@ var mainState = { //create the main state of the gamed
         this.background_music.loop = true;
         this.background_music.play();
 
-        this.lives = 3;
-        this.labelScore = game.add.text(20, 20, "3", {font: "30px Arial", fill: "#ffffff"});
+        //this.labelScore = game.add.text(20, 20, "3", {font: "30px Arial", fill: "#ffffff"});
     },
 
     update:function () { //This is called 60 times per second and contains the game logic
 
         if(this.cursor.left.isDown){
-            this.man.body.velocity.x = -400;
+            man.body.velocity.x = -400;
         }else if (this.cursor.right.isDown){
-            this.man.body.velocity.x = 400;
+            man.body.velocity.x = 400;
         }else{
-            this.man.body.velocity.x = 0;
+            man.body.velocity.x = 0;
         }
 
-        if(this.sword.y > 390){
+        if(sword.y > 390){
 
-            this.sword.y = -50;
-            this.sword.x = Math.floor((Math.random() * 350) + 5);
-            this.sword.body.gravity.y = 5;
+            sword.y = -50;
+            sword.x = Math.floor((Math.random() * 330) + 5);
+            sword.body.gravity.y = 5;
         }
 
-        //if (this.physics.arcade.checkCollision(this.man, this.sword)){
+        game.physics.arcade.overlap(
+            man, sword, this.restartGame, null, this);
+    },
 
-          //  this.lives -= 1;
-          //  this.labelScore.text = this.lives;
-        //}
+    restartGame:function () {
+
+        game.state.start('main');
     }
-
-    /**hitMan:function () {
-
-        if(game.physics.arcade.overlap(this.man, this.sword, null, null, this)){
-
-          this.lives -= 1;
-          this.labelScore.text = this.lives;
-        }
-    }**/
 };
 
 var game = new Phaser.Game(400, 390); //Initializes Phaser and creates a 400px by 490px game
