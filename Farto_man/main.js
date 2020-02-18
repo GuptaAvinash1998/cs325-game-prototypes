e9588dddc3a34f5f0130ffe653de81900c1b737d
Avinash Gupta <avig1998@gmail.com>
2020-02-18 17:57:10 -0500

    Added counter for number of cans collected and attempt of power up

--- Farto_man/main.js
+++ Farto_man/main.js
@@ -6,6 +6,7 @@ var mainState = { //create the main state of the gamed
     
     preload:function () { //This is used to load the images and the sounds and is called at the beginning
 
+        //game.load.image('Background', 'assets/background.png');
         game.load.image('Man', 'assets/Fart_man.png');
         game.load.image('Fart_cloud', 'assets/Fart_cloud.jpg'); //https://www.hiclipart.com/free-transparent-background-png-clipart-ixbvh
         game.load.image('Pipe', 'assets/pipe.png'); //loads the pipe
@@ -16,6 +17,7 @@ var mainState = { //create the main state of the gamed
 
     create:function () { //This is used to set up the game, display the sprites, etc and is called after preload
 
+        //game.add.image('Background');
         game.physics.startSystem(Phaser.Physics.ARCADE); //set the physics system
 
         man = game.add.sprite(100, 245, 'Man'); //this sets the man sprite at position (100, 245)
@@ -29,6 +31,7 @@ var mainState = { //create the main state of the gamed
         this.fart_cloud = game.add.sprite(man.body.x + 40, man.body.y - 30, 'Fart_cloud');
         this.fart_cloud.alpha = 0;
 
+        //var beans = game.add.sprite(200, 145, 'Can_of_beans');
         var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR); //adds functionality to the spacebar
 
         spaceKey.onDown.add(this.jump, this); //This says call the jump function when spacebar is pressed
@@ -37,9 +40,15 @@ var mainState = { //create the main state of the gamed
 
         this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); //calls the addRowOfPipes every 1.5 seconds
 
+        this.beans = game.add.group();
+
         this.score = 0;
 
         this.labelScore = game.add.text(20, 20, "0", {font: "30px Arial", fill: "#ffffff"});
+
+        this.can_count = 0;
+
+        this.labelCount = game.add.text(20, 60, "Cans collected: 0", {font: "30px Arial", fill: "#ffffff"});
 
         this.fartSound = game.add.audio('Fart');
 
@@ -47,10 +56,6 @@ var mainState = { //create the main state of the gamed
 
         this.fart_cloud = game.add.sprite(man.body.x + 10, man.body.y - 30, 'Fart_cloud');
         this.fart_cloud.alpha = 0;
-
-        //game.add.tween(this.fart_cloud).to( { alpha: 1, x: man.body.x - 40, y: man.body.y - 30}, 100, null, true, 0, 0, false);
-
-        //this.timer2=  game.time.events.loop(2000, this.destroy_fart, this);
     },
 
     update:function () { //This is called 60 times per second and contains the game logic
@@ -60,6 +65,7 @@ var mainState = { //create the main state of the gamed
         }
 
         game.physics.arcade.overlap(man, this.pipes, this.hitPipe, null, this);
+        game.physics.arcade.overlap(man, this.beans, this.hitBeans, null, this);
     },
 
     jump:function () {
@@ -74,6 +80,42 @@ var mainState = { //create the main state of the gamed
         this.fartSound.play();
 
     },
+
+    hitBeans:function(){
+
+        game.add.tween(this.can).to( { alpha: 0}, 100, null, true, 0, 0, false);
+
+        this.can_count += 1;
+        this.labelCount.text = "Cans collected: " + (Math.floor(this.can_count/17));
+
+        console.log(Math.floor(this.can_count/17));
+
+        if(Math.floor(this.can_count/17) === 2){
+
+            //man.body.enable = false;
+
+            this.pipes.enableBody = false;
+
+            //game.time.events.add(Phaser.Timer.SECOND*7, this.deactivateShield, this);
+            console.log(Phaser.Timer.SECOND + 60000);
+            game.time.events.add(Phaser.Timer.SECOND + 60000, this.deactivateinvisiPipe(), this);
+            //this.activateBoost();
+        }
+    },
+
+    deactivateinvisiPipe:function(){
+
+        console.log("enabling the pipes group again")
+        this.pipes.enableBody = true;
+        this.can_count = 0;
+    },
+
+    /**deactivateShield:function(){
+
+        man.body.enable = true;
+
+        this.can_count = 0;
+    },**/
 
     hitPipe:function(){
 
@@ -90,6 +132,10 @@ var mainState = { //create the main state of the gamed
         this.pipes.forEach(function(p){
             p.body.velocity.x = 0;
         }, this);
+
+        this.beans.forEach(function (b) {
+            b.body.velocity.x = 0;
+        })
     },
 
     destroy_fart:function(){
@@ -102,24 +148,31 @@ var mainState = { //create the main state of the gamed
 
     addOnePipe:function (x, y) { //This adds a pipe into the world
 
-        var pipe = game.add.sprite(x, y, 'Pipe'); //adds the pipe sprite
+        this.pipe = game.add.sprite(x, y, 'Pipe'); //adds the pipe sprite
 
-        this.pipes.add(pipe); //adds the pipe sprite to the group
+        this.pipes.add(this.pipe); //adds the pipe sprite to the group
 
-        game.physics.arcade.enable(pipe); //enables the physics to the pipe
+        game.physics.arcade.enable(this.pipe); //enables the physics to the pipe
 
-        pipe.body.velocity.x = -200; //add velocity of the pipe to make it move left
+        this.pipe.body.velocity.x = -200; //add velocity of the pipe to make it move left
 
         //kills the pipe when it is out of bounds
-        pipe.checkWorldBounds = true;
-        pipe.outOfBoundsKill = true;
+        this.pipe.checkWorldBounds = true;
+        this.pipe.outOfBoundsKill = true;
     },
 
     addBeans:function(x, y){
 
-        var beans = game.add.sprite(x, y, 'Can_of_beans');
+        this.can = game.add.sprite(x, y, 'Can_of_beans');
 
-        game.physics.enable(beans);
+        this.beans.add(this.can);
+
+        game.physics.enable(this.can);
+
+        this.can.body.velocity.x = -200;
+
+        this.can.checkWorldBounds = true;
+        this.can.outOfBoundsKill = true;
     },
 
     addRowOfPipes: function () { //This adds multiple pipes at the same time
@@ -129,14 +182,17 @@ var mainState = { //create the main state of the gamed
 
         var bean_pop = Math.floor(Math.random() * 5) + 1;
 
+        //console.log(bean_pop);
         for (var i=0; i<8; i++){
 
             if(i !== hole && i !== hole+1){
                 this.addOnePipe(400, i*60+10);
-            }
+            }else{
 
-            if(i === hole || i == hole+1){
-                this.addBeans(400, hole+10);
+                if(i === hole && (bean_pop === 1 || bean_pop === 4)){
+
+                    this.addBeans(400, i*60+40);
+                }
             }
 
             this.score++;
