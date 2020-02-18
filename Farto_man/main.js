23a19e8661ffc4563322a30eac8663d2ee40c60a
Avinash Gupta <avig1998@gmail.com>
2020-02-18 12:21:01 -0500

    Added new sprite and changed fart cloud image

--- Farto_man/main.js
+++ Farto_man/main.js
@@ -5,29 +5,27 @@ var mainState = { //create the main state of the gamed
     
     preload:function () { //This is used to load the images and the sounds and is called at the beginning
 
-        game.load.image('Bird', 'assets/bird.png'); //loads the bird
         game.load.image('Man', 'assets/Fart_man.png');
         game.load.image('Fart_cloud', 'assets/Fart_cloud.jpg'); //https://www.hiclipart.com/free-transparent-background-png-clipart-ixbvh
         game.load.image('Pipe', 'assets/pipe.png'); //loads the pipe
         game.load.audio('Fart', 'assets/short_fart.wav'); //loads the jump music
         game.load.audio('Dead', 'assets/kaboom.wav'); //loads the dead music
+        game.load.image('Can_of_beans', 'assets/Can_of_beans.jpg');
     },
 
     create:function () { //This is used to set up the game, display the sprites, etc and is called after preload
 
-        //game.state.backgroundColor = "#FFFFFF"; //sets the background to blue
-
         game.physics.startSystem(Phaser.Physics.ARCADE); //set the physics system
 
-        man = game.add.sprite(100, 245, 'Man'); //this sets the bird sprite at position (100, 245)
-
-        //man.body.setSize(40, 53, true);
+        man = game.add.sprite(100, 245, 'Man'); //this sets the man sprite at position (100, 245)
         
-        game.physics.arcade.enable(man); //adds physics to the bird
+        game.physics.arcade.enable(man); //adds physics to the man
 
-        man.body.gravity.y = 1000; //add gravity to the bird to make it fall
+        man.body.gravity.y = 1000; //add gravity to the man to make it fall
+		
+		man.body.setSize(40, 53, true);
 
-        this.fart_cloud = game.add.sprite(man.body.x - 40, man.body.y - 30, 'Fart_cloud');
+        this.fart_cloud = game.add.sprite(man.body.x + 40, man.body.y - 30, 'Fart_cloud');
         this.fart_cloud.alpha = 0;
 
         var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR); //adds functionality to the spacebar
@@ -106,10 +104,19 @@ var mainState = { //create the main state of the gamed
         pipe.outOfBoundsKill = true;
     },
 
+    addBeans:function(x, y){
+
+        var beans = game.add.sprite(x, y, 'Can_of_beans');
+
+        game.physics.enable(beans);
+    },
+
     addRowOfPipes: function () { //This adds multiple pipes at the same time
 
         var hole = Math.floor(Math.random() * 5) + 1; //Picks a number between one and 5
         //This is where the hole is going to be for the bird to move through
+
+        var bean_pop = Math.floor(Math.random() * 5) + 1;
 
         for (var i=0; i<8; i++){
 
@@ -117,8 +124,12 @@ var mainState = { //create the main state of the gamed
                 this.addOnePipe(400, i*60+10);
             }
 
+            if(i === hole || i === hole+1){
+                this.addBeans(400, hole+10);
+            }
+
             this.score++;
-            this.labelScore.text = this.score;
+            this.labelScore.text = (this.score / 8) - 1;
         }
     }
 };
